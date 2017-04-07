07f7b75a471df617873e8149b81dc4de1487a5a5
Lisa <gwcgoodnowlibrary@gmail.com>
2017-04-07 13:02:53 +0000

    Lisa branch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309205658) do
+ActiveRecord::Schema.define(version: 20170309222738) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
